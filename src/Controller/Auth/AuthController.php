e97eca6872d7e837ed08768147aff768f2e6986f
Florian Berrotµ <florian.berrot@ynov.com>
2022-01-27 10:16:15 +0100

    feat: add jwt in project

--- src/Controller/Auth/AuthController.php
+++ src/Controller/Auth/AuthController.php
@@ -15,10 +15,9 @@ class AuthController
      */
     public function login(Request $request): JsonResponse
     {
-        FileSystem::upload(file_get_contents($request->files->get('file')),  $request->get('groupe'), $request->get('filename'));
         return new JsonResponse(
             [
-                'message' => 'Upload file ok'
+                'message' => 'User is connected'
             ], 200
         );
     }
